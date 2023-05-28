ffc34fce5b02f8c5561cf4923b91b7bfc20c523e
vinaydawani <vdawani6@gmail.com>
2023-05-27 22:39:25 -0400

    format and actions

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
-from fastapi import FastAPI, APIRouter
-from pydantic import BaseModel
+"""main file"""
+
+from fastapi import APIRouter, FastAPI
 
 from src.schema import LocationData
 
 app = FastAPI(title="Location collection and querying", openapi_url="/openapi.json")
 router = APIRouter()
 
-class data(BaseModel):
-    _type: str
 
 @router.get("/", status_code=200)
 async def root():
     return {"message": "Hello World"}
 
+
 @router.post("/", status_code=201)
 async def collect(*, request: LocationData):
     # zz = await request.body()
     print(request)
+
 
 app.include_router(router=router)
